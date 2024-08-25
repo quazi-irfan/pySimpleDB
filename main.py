f52b9ce514cf82b70343b742e310108bd834b726
Irfan Quazi <quazirfan@gmail.com>
2024-08-25 16:51:55 -0500

    Working 4.11 BufferTest

--- main.py
+++ main.py
@@ -14,10 +14,20 @@
 
 import os
 
+import logging
+import threading
+
+# debug,info,waring,error,critical
+logging.basicConfig(format='{filename}, at line {lineno}, on {threadName} {asctime}: {message}', style='{', level=logging.INFO, datefmt="%H:%M:%S")
+# logging.info('db logging')
+
 class Block:
     def __init__(self, file_name, block_number):
         self.file_name = file_name
         self.block_number = block_number
+
+    def __eq__(self, other):
+        return self.file_name == other.file_name and self.block_number == other.block_number
 
     # implement __eq__, __hash__, __str__
 
@@ -49,7 +59,7 @@ class Page:
             data_bin = data_bin_len + data
 
         data_len = len(data_bin)
-        # Do I need to create + new block to the file for data that exceeds boundary
+        # Do I need to create + new block to the file for data that exceeds boundary?
         self.bb[start:start + data_len] = data_bin
         return data_len 
   
@@ -67,37 +77,49 @@ class Page:
 
 # Purpose of this class is to write a page to a block
 # Read and trigger immediate disk operation( because buffering it set to 0) to ensure data is saved to disk
-# TODO: readBlockToPage, writePageToBlock and appendEmptyBlock methods needs to be synchronized
 class FileMgr:
     # https://stackoverflow.com/questions/1466000/difference-between-modes-a-a-w-w-and-r-in-built-in-open-function
     def __init__(self, db_name, block_size, buffer_size): #As of right now buffer_size is not being used
         self.db_name = db_name,
         self.block_size = block_size
         self.buffer_size = buffer_size
+        self._lock = threading.Lock()
 
     def readBlockToPage(self, block, page):
-        f = open(block.file_name, 'rb', buffering=0) # Does buffering has any effect on reading?
-        f.seek(self.block_size * block.block_number)
-        # Making sure we are only reading the block size of the file
-        # We want to minimize the number of blocks we are reading from the disk
-        # One way query optimize will make plan based on number of potential blocks we need to ready
-        page.bb = bytearray(f.read(self.block_size))
-        f.close()
-    
+        with self._lock:
+            self.length(block.file_name) # TODO: hack to create an emptyÎ file if none exists; this needs to be replaced with a list of file handle that cache opened file handles
+            f = open(block.file_name, 'rb', buffering=0) # Does buffering has any effect on reading?
+            f.seek(self.block_size * block.block_number)
+            # Making sure we are only reading the block size of the file
+            # We want to minimize the number of blocks we are reading from the disk
+            # One way query optimize will make plan based on number of potential blocks we need to ready
+
+            # (I think I am emulating the Java version with this if statement here)
+            # if we are reading 10th block of an empty file; we return a zeroed out page
+            file_content = bytearray(f.read(self.block_size))
+            if file_content:
+                page.bb = file_content
+            else:
+                page.bb = bytearray(self.block_size)
+            f.close()
+
+    # if file does not exist we create a new one
     def writePageToBlock(self, block, page):
-        f = open(block.file_name, 'r+b', buffering=0) # r is used because a prevents seek and w truncates the file
-        f.seek(self.block_size * block.block_number)
-        f.write(page.bb) 
-        f.close()
+        with self._lock:
+            f = open(block.file_name, 'r+b', buffering=0) # r is used because a prevents seek and w truncates the file
+            f.seek(self.block_size * block.block_number)
+            f.write(page.bb)
+            f.close()
 
     # Append a new block to the provided (log) file and return the block reference
     def appendEmptyBlock(self, fileName):
-        f = open(fileName, 'ab', buffering=0) # How does append mode behave if file do not exists?
-        new_block_number = self.length(fileName)
-        f.seek(self.block_size * new_block_number) # seek doesn't with a mode
-        temp_page = Page(self.block_size)
-        f.write(temp_page.bb)
-        f.close()
+        with self._lock:
+            f = open(fileName, 'ab', buffering=0) # How does append mode behave if file do not exists?
+            new_block_number = self.length(fileName)
+            f.seek(self.block_size * new_block_number) # seek doesn't with a mode
+            temp_page = Page(self.block_size)
+            f.write(temp_page.bb)
+            f.close()
         return Block(fileName, new_block_number)
 
     def length(self, file_name):
@@ -197,27 +219,99 @@ class Buffer:
         self.fm = fm
         self.lm = lm
 
-    def contents(self):
-        pass
+        self.block = None
+        self.page = Page(fm.block_size)
+        self.lsn = -1
+        self.txnum = -1
+        self.pin_count = 0
+
+    def setModified(self, txnum, lsn):
+        self.txnum = txnum
+        if lsn >= 0:
+            self.lsn = lsn
+
+    def assignToBlock(self, block):
+        self.flush()
+        self.block = block
+        self.fm.readBlockToPage(block, self.page) # save the requested block to the Buffer's page
+        self.pin_count = 0 # TODO: why we are not incrementing pin count anytime we are reading a block
+
+    def flush(self):
+        if self.txnum >= 0:
+            self.lm.flushPage(self.lsn)
+            self.fm.writePageToBlock(self.block, self.page)
+            self.txnum = -1
+        # else nothing has happened yet, therefore there is nothing to flush
+
+    def pin(self):
+        self.pin_count += 1
+
+    def unpin(self):
+        self.pin_count -= 1
 
 # BufferMgr does two things.
 #   track changes to page(new data) and
 #   (delay) write the modified page back to disk. Write happens when
 #       1. page is getting pined to a diff block,
 #       2. Recovery manager needs to write pages to prevent data loss
+
+# Client ask BufferMgr to pin a block to page
+# block is already in a page
+#   - and that buffer pinned
+#   - and that buffer is not pinned
+# block is not in any page (we have to evict a page)
+#   - all buffer is the buffer pool is pinned
+#   - at least one buffer is the buffer pool is not pinned
 class BufferMgr:
     def __init__(self, fm, lm, num_buffers):
         self.fm = fm
         self.lm = lm
         self.num_buffers = num_buffers
 
-        self.buffers = [Buffer(self.fm, self.lm) for _ in range(self.num_buffers)]
+        self.buffer_pool = [Buffer(self.fm, self.lm) for _ in range(self.num_buffers)]
+        self.pool_availability = self.num_buffers
 
-    def pin(self):
-        pass
+    def pin(self, target_block):
+        # Condition variable will allow u
+        # this needs to be in a thread waited loop
+        return self.tryToPin(target_block)
 
-    def unpin(self):
-        pass
+    def unpin(self, target_buffer):
+        target_buffer.unpin()
+        if not target_buffer.pin_count > 0:
+            self.pool_availability += 1 # No client is using it. New request to pin is now eligible to replace this buffer
+            # notifyAll() # this java method notify any thread waiting for availability in the buffer pool
+
+    def tryToPin(self, target_block):
+        b = self.findExistingBuffer(target_block) # check if the requested block is already present in the buffer pool
+        if not b:
+            b = self.chooseUnpinnedBuffer() # requested block is not already in the buffer pool; so find an unpinned buffer
+            if not b:
+                return None # requested block is neither in buffer pool nor we have any unpinned buffer
+            b.assignToBlock(target_block) # found an unpinned buffer; replace its page with requested block
+
+        # if block was already in buffer pool with pin_count non-zero; we do not lose pool availability yet because someone else was already using it
+        # if block was already in buffer pool with pin_count zero; we still will lose pool availability because we are about to pin the buffer
+        if not b.pin_count > 0:
+            self.pool_availability -= 1
+
+        b.pin()
+        return b
+
+
+    # check if the requested block is already present in the buffer pool
+    def findExistingBuffer(self, target_block):
+        for b in self.buffer_pool:
+            if b.block and (b.block == target_block):
+                return b
+        return None
+
+    # requested block is not already in the buffer pool; so find an unpinned buffer
+    def chooseUnpinnedBuffer(self):
+        for b in self.buffer_pool:
+            if not b.pin_count > 0:
+                return b
+        return None
 
 # Each set of interaction with the database is a transactions
 # Transaction is completed when it has committed or rolledback and released all locks
@@ -225,12 +319,27 @@ class Transaction:
     pass
 
 # Fig 4.11 Testing Buffer
-# fm = FileMgr('simpledb', 400, 8)
-# lm = LogMgr(fm, 'tst_log')
-# bm = BufferMgr(fm, lm, 3)
-# buff1 = bm.pin()
-# buff1 = bm.pin()
-# exit()
+fm = FileMgr('simpledb', 400, 8)
+lm = LogMgr(fm, 'tst_log')
+bm = BufferMgr(fm, lm, 3)
+buff1 = bm.pin(Block('testfile', 1))
+n = buff1.page.getInt(80) # it should return empty because testfile is of size zero
+buff1.page.setData(80, n + 1)
+buff1.setModified(1, 0) # does lsn start at zero?
+print('The new value is ', n+1)
+bm.unpin(buff1) # we do not immediately write it back to disk because some other client might pin it again
+
+buff2 = bm.pin(Block('testfile', 2)) # this write the block 1 back to disk
+buff3 = bm.pin(Block('testfile', 3))
+buff4 = bm.pin(Block('testfile', 4))
+
+bm.unpin(buff2)
+buff11 = bm.pin(Block('testfile', 1))
+buff11.page.setData(80, 9999)
+buff11.setModified(1, 0)
+buff11.unpin() # This modification won't get written to disk because there is noting forcing it
+
+exit()
 
 # Fig 4.12 Testing Buffer Manager
 
